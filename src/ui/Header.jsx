2b0a7afa4eddd29ab6cfcc701a331b65c536f1cd
Mr.Fox <nayeemkhanjim10@gmail.com>
2025-01-08 19:55:35 +0600

    Title fixed

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b-4 border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Mbstu-Pizza-Cart
+        Pizza House
       </Link>
       <SearchOrder />
       <Username />
